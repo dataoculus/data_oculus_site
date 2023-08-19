b4a68927d239a9f7b14613e9be5beac2885025b1
dataoculus <data.oculus.llc@gmail.com>
2023-08-18 23:19:11 -0700

    updating summary image

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,7 +2,6 @@
 import React, {useState} from "react";
 import Image from "next/image";
 
-// import {motion} from "framer-motion";
 import {TypeAnimation} from "react-type-animation";
 
 const Hero = () => {
