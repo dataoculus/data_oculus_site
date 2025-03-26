467a4b145d17dcde463c2deab16846ff8757569a
dataoculus <data.oculus.llc@gmail.com>
2025-03-26 13:07:38 -0700

    local changes for layout

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -58,81 +58,6 @@ const Hero = () => {
             <section className="pt-35 md:pt-40 xl:pt-46 pb-20 xl:pb-25 overflow-hidden">
                 <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
                     <div className="flex lg:items-center lg:gap-4.5 xl:gap-18">
-                        <div className="animate_right md:w-1/2 hidden lg:block pr-10">
-                            <div className="relative 2xl:-mr-7.5">
-                                <Image
-                                    src="/images/shape/shape-01.png"
-                                    alt="shape"
-                                    width={46}
-                                    height={246}
-                                    className="absolute -left-11.5 top-0"
-                                />
-                                <Image
-                                    src="/images/shape/shape-02.svg"
-                                    alt="shape"
-                                    width={36.9}
-                                    height={36.7}
-                                    className="absolute right-0 bottom-0 z-10"
-                                />
-                                <Image
-                                    src="/images/shape/shape-03.svg"
-                                    alt="shape"
-                                    width={21.64}
-                                    height={21.66}
-                                    className="absolute -right-6.5 bottom-0 z-1"
-                                />
-                                <div className=" relative aspect-[700/444] w-full">
-                                    <Image
-                                        className="dark:hidden shadow-solid-l"
-                                        src="/images/hero/dataoculus-summary-light.png"
-                                        alt="Hero"
-                                        fill
-                                        style={{borderRadius: '1rem'}}
-                                    />
-                                    <Image
-                                        className="hidden dark:block shadow-solid-l"
-                                        src="/images/hero/dataoculus-summary-dark.png"
-                                        alt="Hero"
-                                        fill
-                                        style={{borderRadius: '1rem'}}
-                                    />
-                                </div>
-                            </div>
-
-                            {/* <p className="text-black dark:text-white mt-5 text-right text-xs">
-                                Datadog is trademark of <a href="https://www.datadoghq.com/" >Datadog Inc </a>
-                            </p> */}
-
-                            <div className="mt-5  flex float-right mr-50">
-                                {/*<Link*/}
-                                {/*    href="https://cal.com/data-oculus"*/}
-                                {/*    className="flex items-center justify-center bg-primary hover:bg-primaryho ease-in-out duration-300 text-white text-regular rounded-full py-2.5 px-7.5"*/}
-                                {/*>*/}
-                                {/*    Setup Demo 🔥*/}
-                                {/*</Link>*/}
-                                {/*<div className="w-20">*/}
-
-                                {/*</div>*/}
-
-                                {/*<Link*/}
-                                {/*    href="https://public.dataoculus.app"*/}
-                                {/*    className="flex items-center justify-center bg-meta hover:bg-primaryho ease-in-out duration-300 text-white text-regular rounded-full py-2.5 px-7.5"*/}
-                                {/*>*/}
-                                {/*    Public Data Observability*/}
-                                {/*</Link>*/}
-                                <ConfigProvider
-                                    button={{
-                                        className: styles.linearGradientButton,
-                                    }}
-                                >
-                                <Button href="https://public.dataoculus.app/dataset/GCS.kaggle.life-expectancy-who-updated.Life-Expectancy-Data-Updated/summary" type="primary" size="large" >
-                                    Public Dataset Observability
-                                </Button>
-                                </ConfigProvider>
-
-                            </div>
-                        </div>
-
                         <div className="pl-8 md:w-1/2 ">
                             <h4 className="text-black dark:text-white text-lg font-medium mb-4.5">
                                 🔥 Real-Time & End to End
@@ -203,6 +128,10 @@ const Hero = () => {
                                     {/*/>*/}
                                 </span>
                             </h1>
+
+                            <br/>
+
+
                             {/*<br/>*/}
                             {/*<div className="mt-5  flex float-right mr-50">*/}
                             {/*    /!*<Link*!/*/}
@@ -281,6 +210,75 @@ const Hero = () => {
                             {/*    </p>*/}
                             {/*</div>*/}
                         </div>
+
+                        <div className="animate_right md:w-1/2 hidden lg:block pr-10">
+
+                            <div className="relative 2xl:-mr-7.5">
+                                <Image
+                                    src="/images/shape/shape-01.png"
+                                    alt="shape"
+                                    width={46}
+                                    height={246}
+                                    className="absolute -left-11.5 top-0"
+                                />
+                                <Image
+                                    src="/images/shape/shape-02.svg"
+                                    alt="shape"
+                                    width={36.9}
+                                    height={36.7}
+                                    className="absolute right-0 bottom-0 z-10"
+                                />
+                                <Image
+                                    src="/images/shape/shape-03.svg"
+                                    alt="shape"
+                                    width={21.64}
+                                    height={21.66}
+                                    className="absolute -right-6.5 bottom-0 z-1"
+                                />
+                                <div className=" relative aspect-[700/444] w-full">
+                                    <Image
+                                        className="dark:hidden shadow-solid-l"
+                                        src="/images/hero/dataoculus-summary-light.png"
+                                        alt="Hero"
+                                        fill
+                                        style={{borderRadius: '1rem'}}
+                                    />
+                                    <Image
+                                        className="hidden dark:block shadow-solid-l"
+                                        src="/images/hero/dataoculus-summary-dark.png"
+                                        alt="Hero"
+                                        fill
+                                        style={{borderRadius: '1rem'}}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="mt-5 flex float-right ">
+                                <ConfigProvider
+                                    button={{
+                                        className: styles.linearGradientButton,
+                                    }}
+                                >
+                                    <div className="content-center">
+                                        Checkout Demo: &nbsp;
+                                        <Button href="https://public.dataoculus.app/dataset/GCS.kaggle.life-expectancy-who-updated.Life-Expectancy-Data-Updated/summary"
+                                                type="primary" size="large"
+                                                className="ease-in-out duration-300 text-white text-regular rounded-full py-1.5 px-3.5 whitespace-nowrap">
+                                            Public Dataset Observability
+                                        </Button>
+                                    </div>
+
+                                </ConfigProvider>
+                            </div>
+
+
+                            {/* <p className="text-black dark:text-white mt-5 text-right text-xs">
+                                Datadog is trademark of <a href="https://www.datadoghq.com/" >Datadog Inc </a>
+                            </p> */}
+
+
+                        </div>
+
                     </div>
                 </div>
             </section>
